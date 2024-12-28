7ead220865d52a62bc4fed6fd9c554b1966a1449
Rishal-Jr <rmrazni@gmail.com>
2024-12-28 11:39:13 +0530

    hhv

---
+++ A2.py
@@ -0,0 +1,26 @@
+name="Penguin"
+age=15
+is_student=True
+weight=30.8
+
+print("Name:",name)
+print("Data type of Name is: ",type(name))
+
+print("Age:",age)
+print("Data type of Age is: ",type(age))
+
+print("Is_Student:",is_student)
+print("Data type of Is_Student is: ",type(is_student))
+
+print("Weight:",weight)
+print("Data type of Weight is: ",type(weight))
+
+print("\n After Type Casting....")
+age=str(age)
+print(age)
+print("Data type of Name is: ",type(name))
+weight=int(weight)
+print(weight)
+print("Data type of Weight is: ",type(weight))
+
+
---
+++ A3.py
@@ -0,0 +1,3 @@
+text=input("Enter a string")
+revtxt=text[::-1]
+print("Reverse of the given string is: ",revtxt)
